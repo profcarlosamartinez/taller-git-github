b4ff5ba2c119aa5701b63ef613415142e920f0fd
Carlos M <prof.carlos.a.martinez@gmail.com>
2023-04-24 14:20:01 -0300

    Prueba de commit3

--- prueba_bh.sh
+++ prueba_bh.sh
@@ -1,2 +1,4 @@
 echo ¡He aquí un libro de programación shell!  
-echo ¡He aquí un libro de programación shell!  
+echo ¡He aquí un libro de programación shell! 
+
+echo ¡He aquí un libro de programación shell!
